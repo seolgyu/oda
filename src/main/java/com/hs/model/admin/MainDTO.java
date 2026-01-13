9c7dd879445b7d3325add8416aec42c20750da71
user <seolseolseol21@gmail.com>
2026-01-13 17:16:47 +0900

    공지사항 댓글 수정 commit; 김설규

---
+++ src/main/java/com/hs/model/admin/MainDTO.java
@@ -0,0 +1,8 @@
+package com.hs.model.admin;
+
+public class MainDTO {
+	private int membercount;
+	private int postcount;
+	private int communitycount;
+	
+}
--- src/main/java/com/hs/service/admin/NoticeService.java
+++ src/main/java/com/hs/service/admin/NoticeService.java
@@ -35,6 +35,7 @@ public interface NoticeService {
 	public List<NoticeReplyDTO> listReply(Map<String, Object> map);
 	public List<NoticeReplyDTO> listReplyAnswer(Map<String, Object> map);
 	public int replyAnswerCount(Map<String, Object> map);
+	
 	public void updateReplyShowHide(Map<String, Object> map);
 	
 	public Integer hasUserReplyLiked(Map<String, Object> map);
@@ -44,4 +45,7 @@ public interface NoticeService {
 	public int boardLikeCount(long num);
 	
 	public boolean isUserBoardLiked(Map<String, Object> map);
+	
+	public void insertReplyLike (Map<String, Object> map) throws Exception;
+	public Map<String, Object> replyLikeCount(Map<String, Object> map);
 }
